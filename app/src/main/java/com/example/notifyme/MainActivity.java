824d7fac8a39e0e3eaceb846c9b942b8b727f19b
Jose C Morgado Vargas <josecruzmv@outlook.com>
2021-11-19 13:11:01 -0600

    Created notification channel

--- app/src/main/java/com/example/notifyme/MainActivity.java
+++ app/src/main/java/com/example/notifyme/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.notifyme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String PRIMARY_CHANNEL_ID =
+            "primary_notification_channel";
     private Button button_notify;
+    private NotificationManager mNotificationManager;
 
 
     @Override
@@ -26,5 +33,20 @@ public class MainActivity extends AppCompatActivity {
 
     public void sendNotification() {
 
+    }
+
+    public void createNotificationChannel() {
+        mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel( PRIMARY_CHANNEL_ID,
+                    "Mascot Notification", NotificationManager.IMPORTANCE_HIGH);
+
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notification from Mascot");
+            mNotificationManager.createNotificationChannel(notificationChannel);
+
+        }
     }
 }
